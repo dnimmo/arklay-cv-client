151a6c5df94cb283f41a43fe76872f74097c1143
David Nimmo <dnimmo@gmail.com>
2020-04-02 17:01:27 +0100

    very minor styling

--- src/Application/TitleScreen.js
+++ src/Application/TitleScreen.js
@@ -2,13 +2,21 @@ import React, { useContext } from 'react';
 import { ApplicationContext } from './ApplicationContext';
 import Button from '../components/Button';
 
+
+const style = {
+    minHeight: '50vh'
+    , paddingTop: '20vh'
+};
+
+
 const TitleScreen = 
   () => {
       const { startGame } = useContext(ApplicationContext);
 
 
       return (
-          <div>
+          <div
+              style={style}>
               <h1>Project Arklay</h1>
               <Button 
                   onClick={startGame}
