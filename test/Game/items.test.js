6d7b4e6e96509d88589eccd4f4165a01965657b4
David Nimmo <dnimmo@gmail.com>
2020-04-03 14:14:38 +0100

    added item tests

---
+++ test/Game/items.test.js
@@ -0,0 +1,43 @@
+import { useItem } from '../../src/Game/items';
+
+
+describe('Game/items', () => {
+    describe('check to see if item can be used in current room', () => {
+        const availableDirections = 
+          [ { itemsThatCanBeUsed: ['TEST_ITEM'] } ];
+
+
+        test('reports that item can be used in current room', () => {
+            const expected = 
+              true;
+
+
+            const actual =
+              useItem({ 
+                  availableDirections
+                  , item: 'TEST_ITEM'
+              });
+
+
+            expect(actual)
+                .toBe(expected);
+        });
+
+
+        test('reports that item can not be used in current room', () => {
+            const expected = 
+              false;
+
+
+            const actual =
+              useItem({ 
+                  availableDirections
+                  , item: 'TEST_ITEM_TWO'
+              });
+
+
+            expect(actual)
+                .toBe(expected);
+        });
+    });
+});
