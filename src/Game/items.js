4d5c7b22178f672a5d38f27ee8dd29f6a58773b2
David Nimmo <dnimmo@gmail.com>
2020-04-03 14:19:27 +0100

    added test item

--- src/Game/items.js
+++ src/Game/items.js
@@ -10,7 +10,14 @@ const useItem =
 
 
 const items = 
-  {};
+  {
+      TEST_ITEM: { 
+          name: 'Test item'
+          , description: 'An item Nimmo is using to test items...'
+          , messageWhenUsed: 'You used the thing! :)'
+          , messageWhenNotUsed: 'You didn\'t use the thing. :('
+      }
+  };
 
 
 export default items; 
