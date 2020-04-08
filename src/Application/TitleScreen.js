bdcb0d42cd05fba201aa8e793d56901d08d3e189
David Nimmo <dnimmo@gmail.com>
2020-04-08 15:44:42 +0100

    added music

--- src/Application/TitleScreen.js
+++ src/Application/TitleScreen.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import { ApplicationContext } from './ApplicationContext';
 import Button from '../components/Button';
-
+import playMusic from './playMusic';
 
 const styles = {
     container: {
@@ -26,7 +26,11 @@ const TitleScreen =
               <h1>Project Arklay</h1>
               <p style={styles.introText}>Your head hurts. You&apos;re not sure where you are, and you definitely don&apos;t know how you got here. There&apos;s rain thrashing the ground all around you. You figure you might as well try and understand what the Hell is going on...</p>
               <Button 
-                  onClick={startGame}
+                  onClick={() => {
+                      playMusic();
+                      startGame();
+                  }
+                  }
                   text="Start game"
               />
           </div>
